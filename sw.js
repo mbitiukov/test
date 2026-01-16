e4314ab61327e476031de8e4a4aaa13ceb1d2976
mbitiukov <m.bitiukov@eyvi.studio>
2026-01-16 14:37:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE = "eyvi-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+
+  "./website/software.html",
+  "./website/video.html",
+  "./website/experimental.html",
+
+  "./website/softprojects/project1.html",
+  "./website/softprojects/project2.html",
+  "./website/softprojects/project3.html",
+  "./website/softprojects/project4.html",
+
+  "./css/base.css",
+  "./css/home.css",
+  "./css/layout.css",
+  "./css/software.css",
+  "./css/project.css",
+
+  "./assets/icons/logo192.png",
+"./assets/icons/logo512.png",
+
+"./assets/images/banner.jpg",
+"./assets/images/software.jpg",
+"./assets/images/experimental.jpg",
+"./assets/images/video.jpg",
+"./assets/images/p1.jpg",
+"./assets/images/p2.jpg",
+"./assets/images/p3.jpg",
+"./assets/images/p4.jpg",
+
+"./fonts/ppneuebit-bold.otf",
+"./fonts/ppmonowest-regular.otf",
+
+
+  "./js/main.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
